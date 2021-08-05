b4d496db024723d9cf063cf4707f277a0080cefd
vanhopld98 <vanhopld98@gmail.com>
2021-08-05 10:32:20 +0700

    Thuc hanh

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interestRate = 1.0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter investment amount: ");
+        money = scanner.nextDouble();
+
+        System.out.println("Enter number of months: ");
+        month = scanner.nextInt();
+
+        System.out.println("Enter annual interest rate in percentage");
+        interestRate = scanner.nextDouble();
+
+        double totalInterest = 0;
+        for (int i = 0; i < month; i++) {
+            totalInterest += money * (interestRate / 100) / 12 * month;
+        }
+
+        System.out.println("Total of interest " + totalInterest);
+    }
+}
